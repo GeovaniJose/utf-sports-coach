decea7f9e4918cd063f3343ae218cf3277ef1487
Geovani José <geovani-lol@hotmail.com>
2019-05-24 15:31:58 -0300

    Implement LoginPage

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UsuarioService } from './../../services/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public email = '';
+  public senha = '';
+  public checkingUser = true;
+  public showLoading = false;
+
+  constructor(public usuario: UsuarioService, public router: Router) {
+    this.usuario.getUser().subscribe(user => {
+      (user.isOnline)
+        ? this.router.navigateByUrl('/home')
+        : this.checkingUser = false;
+    });
+  }
 
   ngOnInit() {
+  }
+
+  async login() {
+    try {
+      this.showLoading = true;
+      await this.usuario.login(this.email, this.senha);
+      this.showLoading = false;
+      this.router.navigateByUrl('/home');
+    } catch (erro) {
+      this.showLoading = false;
+    }
   }
 
 }
