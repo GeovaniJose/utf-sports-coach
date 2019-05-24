a8cf36639c593be263808efe32549b56506e14d3
Geovani José <geovani-lol@hotmail.com>
2019-05-24 14:28:12 -0300

    Implement FirebaseService

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
-  constructor() { }
+  constructor() {
+    const config = {
+      apiKey: 'AIzaSyD8Rl2mtQIpse7YOSNS3mOhLY9SbScNQ90',
+      authDomain: 'utf-sports.firebaseapp.com',
+      databaseURL: 'https://utf-sports.firebaseio.com',
+      projectId: 'utf-sports',
+      storageBucket: 'utf-sports.appspot.com',
+      messagingSenderId: '209573562359',
+      appId: '1:209573562359:web:7f9f4e6300a2e15b'
+    };
+    firebase.initializeApp(config);
+  }
+
+  db() {
+    return firebase.firestore();
+  }
+
+  auth() {
+    return firebase.auth();
+  }
+
 }
