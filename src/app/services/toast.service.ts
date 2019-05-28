ed8747f46da51199fbb32e12f3e86cf934400eba
Geovani José <geovani-lol@hotmail.com>
2019-05-28 15:00:30 -0300

    Implement ToastService

--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToastService {
 
-  constructor() { }
+  constructor(public toastCtrl: ToastController) { }
+
+  async presentToast(msg) {
+    const toast = await this.toastCtrl.create({
+      message: msg,
+      duration: 2000,
+      position: 'bottom',
+      animated: true
+    });
+    toast.present();
+  }
 }
