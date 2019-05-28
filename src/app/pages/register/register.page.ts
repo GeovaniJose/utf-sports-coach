ed8c62fe0b4ef9210953a0bca269b84a075cd38f
Geovani José <geovani-lol@hotmail.com>
2019-05-28 10:22:10 -0300

    Implement RegisterPage

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UsuarioService } from './../../services/usuario.service';
+import { FirebaseService } from './../../services/firebase.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  public nome = '';
+  public email = '';
+  public telefone = '';
+  public atletica = '';
+  public senha = '';
+  public userId = '';
+  public showLoading = false;
+
+  constructor(public usuario: UsuarioService, public firebase: FirebaseService, public router: Router) {
+    this.usuario.getUser().subscribe(user => {
+      if (user.isOnline) {
+        this.userId = user.id;
+      }
+    });
+  }
 
   ngOnInit() {
+  }
+
+  async registrar() {
+    try {
+      this.showLoading = true;
+      await this.usuario.registrar(this.email, this.senha);
+      this.gravarUsuario();
+      this.showLoading = false;
+      this.router.navigateByUrl('/login');
+    } catch (erro) {
+      this.showLoading = false;
+    }
+  }
+
+  async gravarUsuario() {
+    try {
+      await this.firebase.db().collection('users').doc(this.userId).set({
+        nome: this.nome,
+        telefone: this.telefone,
+        atletica: this.atletica,
+        tipo: 'player'
+      });
+    } catch (erro) {
+      console.log(erro);
+    }
   }
 
 }
