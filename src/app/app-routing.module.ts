325ee203a7a412433fec242b3aa78690230560f1
Geovani José <geovani-lol@hotmail.com>
2019-05-28 10:38:11 -0300

    Generate menu and home pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
+  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
 ];
 
 @NgModule({
